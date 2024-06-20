9bdeb2a9c09fb092f9724a24d7854f7513e53b44
Lakhan Leo <luckybist7821@gmail.com>
2024-06-20 15:24:46 +0530

    layout

---
+++ lib/Common/CommonWidgets.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Widget horizontalSpace(double width){
+  return SizedBox(
+    width: width,
+  );
+}
+Widget verticalSpace(double height){
+  return SizedBox(
+    height: height,
+  );
+}
--- lib/views/screens/home.dart
+++ lib/views/screens/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import '../widgets/appbar.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:wallzy/Common/CommonWidgets.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +10,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<String> WallpaperImageList = [
+    "lib/assets/images/wallpaper1.jpg",
+    "lib/assets/images/wallpaper2.jpeg",
+    "lib/assets/images/wallpaper3.png",
+    "lib/assets/images/wallpaper4.png",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,13 +24,63 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         elevation: 0,
         backgroundColor: Colors.white,
-        title: Appbar(),
+        leading: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Material(
+            elevation: 5,
+            borderRadius: BorderRadius.circular(60),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(60),
+              child: Image.asset(
+                "lib/assets/images/profile_placeholder.png",
+                height: 30,
+                width: 30,
+              ),
+            ),
+          ),
+        ),
+        title: const Padding(
+          padding: EdgeInsets.only(top: 5),
+          child: Text("Wallzy",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w700,fontFamily: "Outfit"),),
+        ),
       ),
-      body: Column(
-        children: [
-          SearchBar(),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20,top: 40,right: 20),
+        child: Column(
+          children: [
+            // verticalSpace(30),
+            CarouselSlider.builder(itemCount: WallpaperImageList.length,
+                itemBuilder: (context, index, realIndex)
+                {
+                  final result = WallpaperImageList[index];
+                  return buildImage(result,index);
+            },
+                options: CarouselOptions(
+                  height: MediaQuery.of(context).size.height/1.5,
+                  autoPlay: true,
+                  aspectRatio: 16/9,
+                  viewportFraction: 1,
+                  enlargeCenterPage: true,
+                  enlargeStrategy: CenterPageEnlargeStrategy.height,
+                )),
+          ],
+        ),
       ),
     );
   }
+  Widget buildImage(String ImageUrl, int index){
+    return Container(
+      height: MediaQuery.of(context).size.height/1.5,
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+         borderRadius: BorderRadius.circular(20),
+          child: Image.asset(ImageUrl,fit: BoxFit.cover,),
+        ),
+      ),
+    );
+
+  }
+
 }
